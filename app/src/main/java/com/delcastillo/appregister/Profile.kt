2c6ef9eb639e6017f4d32c641ab9cd46abb50257
maxbeat9 <dominicrestauro@gmail.com>
2024-05-16 14:34:38 +0800

    updatenasad

--- app/src/main/java/com/delcastillo/appregister/Profile.kt
+++ app/src/main/java/com/delcastillo/appregister/Profile.kt
@@ -1,6 +1,8 @@
 package com.delcastillo.appregister
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,12 @@ class Profile : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_profile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        val profilebtn = findViewById<Button>(R.id.emergency_contacts)
+        profilebtn.setOnClickListener {
+            val intent = Intent(this, EmergencyContact::class.java)
+            startActivity(intent)
         }
     }
 }
