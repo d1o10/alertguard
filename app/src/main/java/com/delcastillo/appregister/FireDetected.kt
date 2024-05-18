2752448b03420e24fdb96960b439e603b848ee77
John Vincent Diocampo <160011885+d1o10@users.noreply.github.com>
2024-05-18 11:26:28 +0800

    Update FireDetected.kt

--- app/src/main/java/com/delcastillo/appregister/FireDetected.kt
+++ app/src/main/java/com/delcastillo/appregister/FireDetected.kt
@@ -1,10 +1,11 @@
 package com.delcastillo.appregister
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
 import android.widget.Button
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
 
 class FireDetected : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,21 +15,19 @@ class FireDetected : AppCompatActivity() {
         val location2: Button = findViewById(R.id.locationIcon)
         location2.setOnClickListener {
             val intent = Intent(this, Location::class.java)
-            startActivity(intent)
+            ContextCompat.startActivity(this, intent, null)
         }
 
         val dashboard: Button = findViewById(R.id.homeIcon)
         dashboard.setOnClickListener {
             val intent = Intent(this, DashboardActivity::class.java)
-            startActivity(intent)
+            ContextCompat.startActivity(this, intent, null)
+        }
 
-            val womanImageView: ImageView = findViewById(R.id.WomanFireDect)
-            womanImageView.setOnClickListener {
-                val intent = Intent(this, Profile::class.java)
-                startActivity(intent)
-            }
+        val womanImageView: ImageView = findViewById(R.id.WomanFireDect)
+        womanImageView.setOnClickListener {
+            val intent = Intent(this, Profile::class.java)
+            ContextCompat.startActivity(this, intent, null)
         }
     }
 }
-
-
