860230d25340ac28a7ec7040b4854e5b58363794
Diocampo <vdiocampo009@gmail.com>
2024-05-18 14:56:55 +0800

    Update karong hapona2

--- app/src/main/java/com/delcastillo/appregister/Location.kt
+++ app/src/main/java/com/delcastillo/appregister/Location.kt
@@ -1,15 +1,22 @@
 package com.delcastillo.appregister
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageView
 
 class Location : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_location)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location)
 
+        val woman: ImageView = findViewById(R.id.Woman)
+        woman.setOnClickListener {
+            val intent = Intent(this, Profile::class.java)
+            startActivity(intent)
+        }
     }
 }
