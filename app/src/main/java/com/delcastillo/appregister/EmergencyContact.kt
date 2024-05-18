71959acac00d90bc498d3bec6031878290f5ae05
Diocampo <vdiocampo009@gmail.com>
2024-05-18 16:23:46 +0800

    Update karong hapona3

--- app/src/main/java/com/delcastillo/appregister/EmergencyContact.kt
+++ app/src/main/java/com/delcastillo/appregister/EmergencyContact.kt
@@ -1,6 +1,8 @@
 package com.delcastillo.appregister
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +12,11 @@ class EmergencyContact : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_emergency_contact)
+
+        val back: Button = findViewById(R.id.backbtn)
+        back.setOnClickListener {
+            val intent = Intent(this, Profile::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/delcastillo/appregister/Settings.kt
+++ app/src/main/java/com/delcastillo/appregister/Settings.kt
@@ -1,6 +1,8 @@
 package com.delcastillo.appregister
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,13 @@ class Settings : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        val back2: Button = findViewById(R.id.backbtn2)
+        back2.setOnClickListener {
+            val intent = Intent(this, Profile::class.java)
+            startActivity(intent)
+        }
+
         }
     }
-}
