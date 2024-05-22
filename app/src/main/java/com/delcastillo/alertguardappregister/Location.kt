86a068ddc0c3de237c95737a759425ebe9a06b81
Diocampo <vdiocampo009@gmail.com>
2024-05-22 21:33:12 +0800

    kapoya3

--- app/src/main/java/com/delcastillo/alertguardappregister/Location.kt
+++ app/src/main/java/com/delcastillo/alertguardappregister/Location.kt
@@ -2,6 +2,7 @@ package com.delcastillo.alertguardappregister
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.ImageView
 import com.delcastillo.appregister.R
@@ -14,6 +15,12 @@ class Location : AppCompatActivity() {
         val woman: ImageView = findViewById(R.id.Woman)
         woman.setOnClickListener {
             val intent = Intent(this, Profile::class.java)
+            startActivity(intent)
+        }
+
+        val locationback: Button = findViewById(R.id.locationbackbtn)
+        locationback.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
             startActivity(intent)
         }
     }
