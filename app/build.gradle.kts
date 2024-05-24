08bfdf695e5c583e9002e3e05ad77ae558a0cb75
Diocampo <vdiocampo009@gmail.com>
2024-05-24 14:44:50 +0800

    update lang

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.13.1")
     implementation ("com.google.firebase:firebase-firestore-ktx:25.0.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
